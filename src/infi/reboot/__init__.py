3b6c5ea5dfb80665c96ca011d0ffd99ec37f4915
Guy Rozendorn <guy@rzn.co.il>
2011-10-10 09:29:17 +0200

    implemented alll methods

--- src/infi/reboot/__init__.py
+++ src/infi/reboot/__init__.py
@@ -2,10 +2,11 @@ __import__("pkg_resources").declare_namespace(__name__)
 
 import os
 import time
+import json
 
 from infi.pyutils.lazy import cached_method
 
-class Reqeust(object):
+class Request(object):
     def __init__(request_key):
         super(Request, self).__init__()
         self._name = name
@@ -29,36 +30,43 @@ class Reqeust(object):
             return True
 
     def _get_expected_uptime_if_no_reboot_ocurred(self):
-        return self._get_uptime_from_key_file() + self._get_current_time() - self._get_time_from_key_file() 
+        return self._get_uptime_from_key_file() + self._get_current_time() - self._get_time_from_key_file()
 
-    def get_snapshot_from_key_file(self):
-        raise NotImplementedError()
+    def _get_snapshot_from_key_file(self):
+        with open(self._get_key_filepath(), 'r') as fd:
+            resultdict = json.load(fd)
+            return resultdict
 
     def _get_time_from_key_file(self):
-        raise NotImplementedError()
+        return self._get_snapshot_from_key_file['time']
 
     def _get_uptime_from_key_file(self):
-        raise NotImplementedError()
+        return self._get_snapshot_from_key_file['uptime']
 
     def _get_current_time(self):
         return time.time()
 
+    def _get_system_boot_time(self):
+        from psutil import BOOT_TIME
+        return BOOT_TIME
+
     def _get_current_uptime(self):
-        raise NotImplementedError()
+        return self._get_current_time() - self._get_current_time()
 
     def make_request(self):
         self._remove_key_file()
-        self._create_key_file()
         self._write_snapshot_to_key_file()
 
     def _remove_key_file(self):
-        raise NotImplementedError()
-
-    def _create_key_file(self):
-        raise NotImplementedError()
+        path = self._get_key_filepath()
+        if not os.path.exists(path):
+            os.remove(path)
 
     def _write_snapshot_to_key_file(self):
-        raise NotImplementedError()
+        path = self._get_key_filepath()
+        with open(path, 'w') as fd:
+            json.dump(dict(uptime=self._get_current_uptime(),
+                           time=self._get_current_time(), indent=4), fd)
 
 def ask_for_reboot(key):
     Request(key).make_request()
